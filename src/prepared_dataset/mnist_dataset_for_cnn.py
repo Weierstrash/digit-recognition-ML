e857409b5f97fd4850f4f42e180a8b222eed6a90
Aravind <arav.erode@gmail.com>
2023-01-29 19:02:34 +0100

    made some minor changes

---
+++ src/prepared_dataset/mnist_dataset_for_cnn.py
@@ -0,0 +1,12 @@
+from keras.datasets import mnist
+from keras.utils import to_categorical
+
+(train_images,train_labels),(test_images,test_labels) = mnist.load_data()
+
+train_images = train_images.reshape((60000,28,28,1))
+train_images = train_images.astype('float32')/255
+test_images = test_images.reshape((60000,28,28,1))
+test_images = test_images.astype('float32')/255
+
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
---
+++ train_model_cnn.py
@@ -0,0 +1,25 @@
+from src.CNN_network.digit_recognition_cnn import model
+from src.prepared_dataset.mnist_dataset_for_cnn import train_images
+from src.prepared_dataset.mnist_dataset_for_cnn import train_labels
+import keras
+
+#compiling the model
+model.compile(optimizer = 'rmsprop',
+              loss = 'categorical_crossentropy',
+              metrics = ['accuracy'])
+
+
+if __name__ == "__main__":
+    #fitting the model
+    model.fit(train_images , 
+            train_labels, 
+            epochs = 5,
+            batch_size = 64)
+
+    model.save('src/saved_model_cnn')
+
+
+
+
+    
+
